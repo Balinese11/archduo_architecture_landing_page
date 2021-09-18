e1e010404be08b4662815e2fd1c85bfdd81b44cf
Bali <70278134+Balinese11@users.noreply.github.com>
2021-09-18 16:17:21 +0200

    Add files via upload
    Added files to the Archduo repository.

---
+++ archduo/js/script-main.js
@@ -0,0 +1,62 @@
+
+// Testimonial
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" color-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " color-active";
+}
+
+//----------- Testimonial
+
+
+
+
+const counters = document.querySelectorAll('.counter');
+const speed = 400; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+            counter.innerText = Math.ceil(count + inc);
+			// Call function every ms
+			setTimeout(updateCount, 5);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
